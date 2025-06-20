193833ca991799e9b3507f12b36264aab845611a
andylokandy <andylokandy@hotmail.com>
2025-06-20 15:09:00 +0800

    fix

--- src/sptr.rs
+++ src/sptr.rs
@@ -1,6 +1,9 @@
 #[cfg(feature = "nightly")]
+#[allow(clippy::incompatible_msrv)]
 mod implementation {
-    pub use core::ptr::without_provenance_mut;
+    pub const fn without_provenance_mut<T>(addr: usize) -> *mut T {
+        core::ptr::without_provenance_mut(addr)
+    }
 
     pub fn with_metadata_of<T: ?Sized, U: ?Sized>(ptr: *const T, meta: *const U) -> *const U {
         ptr.with_metadata_of(meta)
